11c34caebe94d575d9bf3bf338f3bcef17fc3166
xMaksIce <125661351+xMaksIce@users.noreply.github.com>
2023-04-24 12:29:36 +0600

    Add files via upload

---
+++ Obobshcheniya.cs
@@ -0,0 +1,44 @@
+﻿Massiv<string> mass = new Massiv<string>(5);
+mass.Set("aboba", 0);
+
+Console.WriteLine(mass.Get(0));
+mass.Remove(0);
+
+Console.WriteLine(mass.Get(0));
+
+Massiv<int> massiv2 = new Massiv<int>(2);
+massiv2.Set(12435, 0);
+massiv2.Set(-134, 1);
+massiv2.Print();
+massiv2.Remove(0);
+massiv2.Print();
+
+class Massiv<T>
+{
+    T[] ar;
+    
+    public Massiv(int length) 
+    { 
+        this.ar = new T[length];
+    }
+    public void Set (T el, int index)
+    {
+        ar[index] = el;
+    }
+    public T Get(int index)
+    {
+        return ar[index];
+    }
+    public void Remove(int index)
+    {
+        ar[index] = default(T);
+    }
+    public void Print()
+    {
+        foreach(T el in ar)
+        {
+            Console.Write(el + " ");
+        }
+        Console.WriteLine();
+    }
+}
