9cf844637cac57d0a464b73923941fdacf4de4ab
xMaksIce <125661351+xMaksIce@users.noreply.github.com>
2023-04-27 09:41:11 +0600

    Add files via upload

---
+++ Lambda(Max,Min,...).cs
@@ -0,0 +1,9 @@
+﻿// с использованием делегата реализовать лямбда-выражение минимального, максимального, суммы, произведения и ср. арифметического трёх чисел
+// один делегат со всеми методами
+Action<double, double, double> operations = (a, b, c) => Console.WriteLine("Минимальное: " + Math.Min(Math.Min(a, b), c));
+operations += (a, b, c) => Console.WriteLine("Максимальное: " + Math.Max(Math.Max(a, b), c));
+operations += (a, b, c) => Console.WriteLine("Сумма: " + (a + b + c));
+operations += (a, b, c) => Console.WriteLine("Произведение: " + a * b * c);
+operations += (a, b, c) => Console.WriteLine("Среднее арифметическое: " + (a + b + c) / 3);
+
+operations(3, 4, 5);
