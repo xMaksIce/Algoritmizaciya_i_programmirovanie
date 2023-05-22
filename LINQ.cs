957a95372a30e354fe0b2a5425ff27ceecf6ed62
xMaksIce <125661351+xMaksIce@users.noreply.github.com>
2023-05-22 11:40:37 +0600

    Update LINQ.cs

--- LINQ.cs
+++ LINQ.cs
@@ -1,14 +1,15 @@
-﻿// есть массив, необходимо определить сумму положительных, произведение ненулевых, колво чётных. затем изменить массив, удалить все нечётные
+// есть массив, необходимо определить сумму положительных, произведение ненулевых, колво чётных. затем изменить массив, удалить все нечётные
 // выполнить те же запросы.
 
-double[] firstNums = {1, 2, 3, -31, 0, 0.314, -341, 0.4444 };
-Requests(firstNums);
+List<double> nums = new List<double>() {1, 2, 3, -31, 0, 0.314, -341, 0.4444 };
+Requests(nums);
 // выборка без нечётных
-var secondNums =
-    from num in firstNums
+var needToDelete =
+    from num in nums
     where num % 2 != 1
     select num;
-Requests(secondNums);
+foreach (double num in needToDelete.ToArray()) nums.Remove(num);
+Requests(nums);
 void Requests(IEnumerable<double> nums)
 {
     var Positives =
@@ -23,16 +24,15 @@ void Requests(IEnumerable<double> nums)
         from num in nums
         where num != 0
         select num;
-    double multOfNotNull = 0;
-    foreach (var num in notNull) multOfNotNull += num;
+    double multOfNotNull = 1;
+    foreach (var num in notNull) multOfNotNull *= num;
     Console.WriteLine(multOfNotNull);
 
     var even =
         from num in nums
         where num % 2 == 0
         select num;
-    int amountOfEven = 0;
-    foreach (var num in even) amountOfEven++;
+    int amountOfEven = even.Count();
     Console.WriteLine(amountOfEven);
 
     Console.WriteLine();
